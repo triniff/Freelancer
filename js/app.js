d6b044c0d1f76dbeb036f0f903c3b2c5862c9e3f
“Trinidad <tfuentesfuentealba@gmail.com>
2017-06-01 18:50:05 -0300

    menu

--- js/app.js
+++ js/app.js
@@ -3,11 +3,21 @@
  */
 
  //CREANDO MENU
- window.addEventListener("scroll", function(){
- 	var menu = document.getElementsByTagName("nav")[0];
- 	menu.classList.add("scroll-menu");
-	var header = document.getElementsByTagName("header")[0];
- 	if(){
+ window.addEventListener('scroll', function () {
+   const distanceY = window.pageYOffset || document.documentElement.scrollTop,
+   shrinkOn = 200,
+   headerEl = document.getElementsByTagName("nav")[0];
 
- 	}
- })
+   if (distanceY > shrinkOn) {
+     headerEl.classList.add("scroll-menu");
+   } else {
+     headerEl.classList.remove("scroll-menu");
+   }
+
+   var portfolio = document.getElementById("portfolio");
+   var menu = document.getElementsByClassName("portfolio")[0];
+   if(distanceY > shrinkOn){
+   		menu.classList.add("portfolio-scroll");
+   }
+ });
+
