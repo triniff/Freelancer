4ebf50b93c6f48303b9e88544fd4223ca10d9ca1
Trinidad Fuentes <tfuentesfuentealba@gmail.com>
2017-06-01 14:18:12 -0400

    menu scroll

--- js/app.js
+++ js/app.js
@@ -1,3 +1,13 @@
 /*
  * Archivo principal de JS
  */
+
+ //CREANDO MENU
+ window.addEventListener("scroll", function(){
+ 	var menu = document.getElementsByTagName("nav")[0];
+ 	menu.classList.add("scroll-menu");
+	var header = document.getElementsByTagName("header")[0];
+ 	if(){
+
+ 	}
+ })
